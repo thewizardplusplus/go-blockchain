6c4ca69fcd3187e2637e24ec692e39d277f93e38
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-05 23:00:00 +0300

    Add the blockchain.Block structure

--- block.go
+++ block.go
@@ -1,6 +1,18 @@
 package blockchain
 
+import (
+	"time"
+)
+
 // Hasher ...
 type Hasher interface {
 	Hash() string
+}
+
+// Block ...
+type Block struct {
+	Timestamp time.Time
+	Data      Hasher
+	Hash      string
+	PrevHash  string
 }
