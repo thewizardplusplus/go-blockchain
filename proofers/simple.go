30dce62fe26226d1b446e9be5062eba64a210c44
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-05 23:00:00 +0300

    Add the proofers.Simple.Hash() method

--- proofers/simple.go
+++ proofers/simple.go
@@ -1,4 +1,18 @@
 package proofers
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/thewizardplusplus/go-blockchain"
+)
+
 // Simple ...
 type Simple struct{}
+
+// Hash ...
+func (proofer Simple) Hash(block blockchain.Block) string {
+	data := block.Timestamp.String() + block.Data.Hash() + block.PrevHash
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
+}
