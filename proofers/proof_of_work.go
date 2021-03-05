0ca21e32d5cc89b83adf9b15d606acfabff19e5f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-05 23:00:00 +0300

    Add the proofers.ProofOfWork.Hash() method

--- proofers/proof_of_work.go
+++ proofers/proof_of_work.go
@@ -1,6 +1,41 @@
 package proofers
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+
+	"github.com/thewizardplusplus/go-blockchain"
+)
+
 // ProofOfWork ...
 type ProofOfWork struct {
 	TargetBit int
+}
+
+// Hash ...
+func (proofer ProofOfWork) Hash(block blockchain.Block) string {
+	var target big.Int
+	target.SetBit(&target, proofer.TargetBit, 1)
+
+	var nonce big.Int
+	var hash [sha256.Size]byte
+	for {
+		data := block.Timestamp.String() +
+			block.Data.Hash() +
+			block.PrevHash +
+			nonce.String()
+		hash = sha256.Sum256([]byte(data))
+
+		hashAsInt := big.NewInt(0)
+		hashAsInt.SetBytes(hash[:])
+
+		if hashAsInt.Cmp(&target) == -1 /* is less */ {
+			break
+		}
+
+		nonce.Add(&nonce, big.NewInt(1)) // nonce += 1
+	}
+
+	return fmt.Sprintf("%s:%x", &nonce, hash)
 }
