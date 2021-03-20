97564ee760da515fdd9afe07aa4d993e673a4013
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-20 23:00:00 +0300

    Generate a mock for the blockchain.Storage interface

--- blockchain.go
+++ blockchain.go
@@ -7,6 +7,8 @@ import (
 // ErrEmptyStorage ...
 var ErrEmptyStorage = errors.New("empty storage")
 
+//go:generate mockery --name=Storage --inpackage --case=underscore --testonly
+
 // Storage ...
 type Storage interface {
 	LoadLastBlock() (Block, error)
