ab845cadb77deafe064d935a8f79513dd69d251a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-21 23:00:00 +0300

    Add basics tests for the blockchain.Blockchain.AddBlock() method

--- blockchain_test.go
+++ blockchain_test.go
@@ -177,3 +177,41 @@ func TestNewBlockchain(test *testing.T) {
 		})
 	}
 }
+
+func TestBlockchain_AddBlock(test *testing.T) {
+	type fields struct {
+		dependencies Dependencies
+		lastBlock    Block
+	}
+	type args struct {
+		data Hasher
+	}
+
+	for _, data := range []struct {
+		name          string
+		fields        fields
+		args          args
+		wantLastBlock Block
+		wantErr       assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(t *testing.T) {
+			blockchain := &Blockchain{
+				dependencies: data.fields.dependencies,
+				lastBlock:    data.fields.lastBlock,
+			}
+			gotErr := blockchain.AddBlock(data.args.data)
+
+			mock.AssertExpectationsForObjects(
+				test,
+				data.fields.dependencies.Proofer,
+				data.fields.dependencies.Storage,
+				data.fields.lastBlock.Data,
+				blockchain.lastBlock.Data,
+			)
+			assert.Equal(test, data.wantLastBlock, blockchain.lastBlock)
+			data.wantErr(test, gotErr)
+		})
+	}
+}
