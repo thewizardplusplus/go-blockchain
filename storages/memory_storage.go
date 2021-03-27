1757ca559ef77a064f5aa70d8965ff439e757b30
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-27 23:00:00 +0300

    Add the storages.MemoryStorage.LoadLastBlock() method

--- storages/memory_storage.go
+++ storages/memory_storage.go
@@ -8,3 +8,13 @@ import (
 type MemoryStorage struct {
 	blocks []blockchain.Block
 }
+
+// LoadLastBlock ...
+func (storage MemoryStorage) LoadLastBlock() (blockchain.Block, error) {
+	if len(storage.blocks) == 0 {
+		return blockchain.Block{}, blockchain.ErrEmptyStorage
+	}
+
+	lastBlock := storage.blocks[len(storage.blocks)-1]
+	return lastBlock, nil
+}
