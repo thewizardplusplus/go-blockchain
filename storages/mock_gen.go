5aa5cb0586c169220854a50a75705377c8c5e3bf
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-27 23:00:00 +0300

    Generate a mock for the blockchain.Hasher interface in the storages package

---
+++ storages/mock_gen.go
@@ -0,0 +1,15 @@
+package storages
+
+import (
+	"github.com/thewizardplusplus/go-blockchain"
+)
+
+//go:generate mockery --name=Hasher --inpackage --case=underscore --testonly
+
+// Hasher ...
+//
+// It's used only for mock generating.
+//
+type Hasher interface {
+	blockchain.Hasher
+}
