51207c168b4c36c3fcf998cf2124da048891aa3a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-08 23:00:00 +0300

    Add the blockchain.BlockGroup.IsValid() method

--- block_group.go
+++ block_group.go
@@ -2,3 +2,20 @@ package blockchain
 
 // BlockGroup ...
 type BlockGroup []Block
+
+// IsValid ...
+func (blocks BlockGroup) IsValid(dependencies BlockDependencies) bool {
+	lastIndex := len(blocks) - 1
+	for index, block := range blocks[:lastIndex] {
+		prevBlock := blocks[index+1]
+		if !block.IsValid(prevBlock, dependencies) {
+			return false
+		}
+	}
+
+	if !blocks[lastIndex].IsValidGenesisBlock(dependencies) {
+		return false
+	}
+
+	return true
+}
