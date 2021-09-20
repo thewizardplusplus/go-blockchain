77b79a82c1441af0db971bb7ca5faab16840d996
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-20 23:00:00 +0300

    Implement tests of the blockchain.GroupStorageWrapper.StoreBlockGroup() method

--- group_storage_test.go
+++ group_storage_test.go
@@ -2,6 +2,8 @@ package blockchain
 
 import (
 	"testing"
+	"testing/iotest"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -21,7 +23,114 @@ func TestGroupStorageWrapper_StoreBlockGroup(test *testing.T) {
 		args    args
 		wantErr assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success without blocks",
+			fields: fields{
+				Storage: new(MockStorage),
+			},
+			args: args{
+				blocks: nil,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "success with blocks",
+			fields: fields{
+				Storage: func() Storage {
+					blocks := BlockGroup{
+						{
+							Timestamp: clock(),
+							Data:      new(MockHasher),
+							Hash:      "hash #1",
+							PrevHash:  "",
+						},
+						{
+							Timestamp: clock().Add(time.Hour),
+							Data:      new(MockHasher),
+							Hash:      "hash #2",
+							PrevHash:  "hash #1",
+						},
+						{
+							Timestamp: clock().Add(2 * time.Hour),
+							Data:      new(MockHasher),
+							Hash:      "hash #3",
+							PrevHash:  "hash #2",
+						},
+					}
+
+					storage := new(MockStorage)
+					for _, block := range blocks {
+						storage.On("StoreBlock", block).Return(nil)
+					}
+
+					return storage
+				}(),
+			},
+			args: args{
+				blocks: BlockGroup{
+					{
+						Timestamp: clock(),
+						Data:      new(MockHasher),
+						Hash:      "hash #1",
+						PrevHash:  "",
+					},
+					{
+						Timestamp: clock().Add(time.Hour),
+						Data:      new(MockHasher),
+						Hash:      "hash #2",
+						PrevHash:  "hash #1",
+					},
+					{
+						Timestamp: clock().Add(2 * time.Hour),
+						Data:      new(MockHasher),
+						Hash:      "hash #3",
+						PrevHash:  "hash #2",
+					},
+				},
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "error",
+			fields: fields{
+				Storage: func() Storage {
+					block := Block{
+						Timestamp: clock(),
+						Data:      new(MockHasher),
+						Hash:      "hash #1",
+						PrevHash:  "",
+					}
+
+					storage := new(MockStorage)
+					storage.On("StoreBlock", block).Return(iotest.ErrTimeout)
+
+					return storage
+				}(),
+			},
+			args: args{
+				blocks: BlockGroup{
+					{
+						Timestamp: clock(),
+						Data:      new(MockHasher),
+						Hash:      "hash #1",
+						PrevHash:  "",
+					},
+					{
+						Timestamp: clock().Add(time.Hour),
+						Data:      new(MockHasher),
+						Hash:      "hash #2",
+						PrevHash:  "hash #1",
+					},
+					{
+						Timestamp: clock().Add(2 * time.Hour),
+						Data:      new(MockHasher),
+						Hash:      "hash #3",
+						PrevHash:  "hash #2",
+					},
+				},
+			},
+			wantErr: assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			wrapper := GroupStorageWrapper{
