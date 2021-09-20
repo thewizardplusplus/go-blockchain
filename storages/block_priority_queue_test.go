98181da25623d8c9e8b7d345d566025786fa7a29
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-20 23:00:00 +0300

    Add basics of tests for the storages.BlockPriorityQueue.Less() method

--- storages/block_priority_queue_test.go
+++ storages/block_priority_queue_test.go
@@ -37,3 +37,28 @@ func TestBlockPriorityQueue_Len(test *testing.T) {
 	}
 	assert.Equal(test, 3, gotLength)
 }
+
+func TestBlockPriorityQueue_Less(test *testing.T) {
+	type args struct {
+		i int
+		j int
+	}
+
+	for _, data := range []struct {
+		name       string
+		queue      BlockPriorityQueue
+		args       args
+		wantIsLess assert.BoolAssertionFunc
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			isLess := data.queue.Less(data.args.i, data.args.j)
+
+			for _, block := range data.queue {
+				mock.AssertExpectationsForObjects(test, block.Data)
+			}
+			data.wantIsLess(test, isLess)
+		})
+	}
+}
