27526a93c1d095275e334c9b74ef0127d72f67f9
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-20 23:00:00 +0300

    Add the test for the storages.BlockPriorityQueue.Len() method

---
+++ storages/block_priority_queue_test.go
@@ -0,0 +1,39 @@
+package storages
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/thewizardplusplus/go-blockchain"
+)
+
+func TestBlockPriorityQueue_Len(test *testing.T) {
+	queue := BlockPriorityQueue(blockchain.BlockGroup{
+		{
+			Timestamp: clock(),
+			Data:      new(MockHasher),
+			Hash:      "hash #1",
+			PrevHash:  "",
+		},
+		{
+			Timestamp: clock().Add(time.Hour),
+			Data:      new(MockHasher),
+			Hash:      "hash #2",
+			PrevHash:  "hash #1",
+		},
+		{
+			Timestamp: clock().Add(2 * time.Hour),
+			Data:      new(MockHasher),
+			Hash:      "hash #3",
+			PrevHash:  "hash #2",
+		},
+	})
+	gotLength := queue.Len()
+
+	for _, block := range queue {
+		mock.AssertExpectationsForObjects(test, block.Data)
+	}
+	assert.Equal(test, 3, gotLength)
+}
