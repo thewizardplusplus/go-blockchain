f37bcc8ba1499d4205b2436a1d9265d50c2a933f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-20 23:00:00 +0300

    Add the blockchain.GroupStorageWrapper.StoreBlockGroup() method

--- group_storage.go
+++ group_storage.go
@@ -1,5 +1,9 @@
 package blockchain
 
+import (
+	"github.com/pkg/errors"
+)
+
 // GroupStorage ...
 type GroupStorage interface {
 	Storage
@@ -10,4 +14,15 @@ type GroupStorage interface {
 // GroupStorageWrapper ...
 type GroupStorageWrapper struct {
 	Storage
+}
+
+// StoreBlockGroup ...
+func (wrapper GroupStorageWrapper) StoreBlockGroup(blocks BlockGroup) error {
+	for index, block := range blocks {
+		if err := wrapper.StoreBlock(block); err != nil {
+			return errors.Wrapf(err, "unable to store block #%d", index)
+		}
+	}
+
+	return nil
 }
