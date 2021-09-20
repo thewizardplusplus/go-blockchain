0ee0e8318fa6d75b258ae93fdfea299504d04ed8
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-20 23:00:00 +0300

    Add the storages.BlockPriorityQueue.Swap() method

--- storages/block_priority_queue.go
+++ storages/block_priority_queue.go
@@ -17,3 +17,8 @@ func (queue BlockPriorityQueue) Less(i int, j int) bool {
 	// use the descending order to the pop operation will return the last block
 	return queue[i].Timestamp.After(queue[j].Timestamp)
 }
+
+// Swap ...
+func (queue BlockPriorityQueue) Swap(i int, j int) {
+	queue[i], queue[j] = queue[j], queue[i]
+}
