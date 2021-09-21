762e6c134163f83c6fa666ea9caf15dfc423333f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-21 23:00:00 +0300

    Implement tests of the blockchain.ChunkValidatingLoader.LoadBlocks() method

--- chunk_validating_loader_test.go
+++ chunk_validating_loader_test.go
@@ -2,6 +2,8 @@ package blockchain
 
 import (
 	"testing"
+	"testing/iotest"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -25,7 +27,173 @@ func TestChunkValidatingLoader_LoadBlocks(test *testing.T) {
 		wantNextCursor interface{}
 		wantErr        assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success without blocks",
+			fields: fields{
+				Loader: func() Loader {
+					loader := new(MockLoader)
+					loader.On("LoadBlocks", "cursor-one", 23).Return(nil, "cursor-two", nil)
+
+					return loader
+				}(),
+				Dependencies: BlockDependencies{
+					Clock:   clock,
+					Proofer: new(MockProofer),
+				},
+			},
+			args: args{
+				cursor: "cursor-one",
+				count:  23,
+			},
+			wantBlocks:     nil,
+			wantNextCursor: "cursor-two",
+			wantErr:        assert.NoError,
+		},
+		{
+			name: "success with blocks",
+			fields: fields{
+				Loader: func() Loader {
+					blocks := BlockGroup{
+						{
+							Timestamp: clock().Add(time.Hour),
+							Data:      new(MockHasher),
+							Hash:      "next hash",
+							PrevHash:  "hash",
+						},
+						{
+							Timestamp: clock(),
+							Data:      new(MockHasher),
+							Hash:      "hash",
+							PrevHash:  "previous hash",
+						},
+					}
+
+					loader := new(MockLoader)
+					loader.On("LoadBlocks", "cursor-one", 23).Return(blocks, "cursor-two", nil)
+
+					return loader
+				}(),
+				Dependencies: BlockDependencies{
+					Clock: clock,
+					Proofer: func() Proofer {
+						blocks := BlockGroup{
+							{
+								Timestamp: clock().Add(time.Hour),
+								Data:      new(MockHasher),
+								Hash:      "next hash",
+								PrevHash:  "hash",
+							},
+							{
+								Timestamp: clock(),
+								Data:      new(MockHasher),
+								Hash:      "hash",
+								PrevHash:  "previous hash",
+							},
+						}
+
+						proofer := new(MockProofer)
+						for _, block := range blocks {
+							proofer.On("Validate", block).Return(true)
+						}
+
+						return proofer
+					}(),
+				},
+			},
+			args: args{
+				cursor: "cursor-one",
+				count:  23,
+			},
+			wantBlocks: BlockGroup{
+				{
+					Timestamp: clock().Add(time.Hour),
+					Data:      new(MockHasher),
+					Hash:      "next hash",
+					PrevHash:  "hash",
+				},
+				{
+					Timestamp: clock(),
+					Data:      new(MockHasher),
+					Hash:      "hash",
+					PrevHash:  "previous hash",
+				},
+			},
+			wantNextCursor: "cursor-two",
+			wantErr:        assert.NoError,
+		},
+		{
+			name: "error with block loading",
+			fields: fields{
+				Loader: func() Loader {
+					loader := new(MockLoader)
+					loader.
+						On("LoadBlocks", "cursor-one", 23).
+						Return(nil, "", iotest.ErrTimeout)
+
+					return loader
+				}(),
+				Dependencies: BlockDependencies{
+					Clock:   clock,
+					Proofer: new(MockProofer),
+				},
+			},
+			args: args{
+				cursor: "cursor-one",
+				count:  23,
+			},
+			wantBlocks:     nil,
+			wantNextCursor: nil,
+			wantErr:        assert.Error,
+		},
+		{
+			name: "error with block validating",
+			fields: fields{
+				Loader: func() Loader {
+					blocks := BlockGroup{
+						{
+							Timestamp: clock().Add(time.Hour),
+							Data:      new(MockHasher),
+							Hash:      "next hash",
+							PrevHash:  "hash",
+						},
+						{
+							Timestamp: clock(),
+							Data:      new(MockHasher),
+							Hash:      "hash",
+							PrevHash:  "previous hash",
+						},
+					}
+
+					loader := new(MockLoader)
+					loader.On("LoadBlocks", "cursor-one", 23).Return(blocks, "cursor-two", nil)
+
+					return loader
+				}(),
+				Dependencies: BlockDependencies{
+					Clock: clock,
+					Proofer: func() Proofer {
+						block := Block{
+							Timestamp: clock().Add(time.Hour),
+							Data:      new(MockHasher),
+							Hash:      "next hash",
+							PrevHash:  "hash",
+						}
+
+						proofer := new(MockProofer)
+						proofer.On("Validate", block).Return(false)
+
+						return proofer
+					}(),
+				},
+			},
+			args: args{
+				cursor: "cursor-one",
+				count:  23,
+			},
+			wantBlocks:     nil,
+			wantNextCursor: nil,
+			wantErr:        assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			loader := ChunkValidatingLoader{
