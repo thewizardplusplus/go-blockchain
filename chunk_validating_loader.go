36c2e34a5ef3a72bb85ac826173c49673240aeb9
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-21 23:00:00 +0300

    Add the blockchain.ChunkValidatingLoader.LoadBlocks() method

--- chunk_validating_loader.go
+++ chunk_validating_loader.go
@@ -1,7 +1,30 @@
 package blockchain
 
+import (
+	"github.com/pkg/errors"
+)
+
 // ChunkValidatingLoader ...
 type ChunkValidatingLoader struct {
 	Loader       Loader
 	Dependencies BlockDependencies
+}
+
+// LoadBlocks ...
+func (loader ChunkValidatingLoader) LoadBlocks(cursor interface{}, count int) (
+	blocks BlockGroup,
+	nextCursor interface{},
+	err error,
+) {
+	blocks, nextCursor, err = loader.Loader.LoadBlocks(cursor, count)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if !blocks.IsValid(nil, AsBlockchainChunk, loader.Dependencies) {
+		const message = "the blocks corresponding to cursor %v are not valid"
+		return nil, nil, errors.Errorf(message, cursor)
+	}
+
+	return blocks, nextCursor, nil
 }
