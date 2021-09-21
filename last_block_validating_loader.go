f03ac1020f7d087991cc5d33db40d612e09a10c0
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-21 23:00:00 +0300

    Add the blockchain.LastBlockValidatingLoader.LoadBlocks() method

--- last_block_validating_loader.go
+++ last_block_validating_loader.go
@@ -1,7 +1,52 @@
 package blockchain
 
+import (
+	"github.com/pkg/errors"
+)
+
 // LastBlockValidatingLoader ...
 type LastBlockValidatingLoader struct {
 	Loader       Loader
 	Dependencies BlockDependencies
+}
+
+// LoadBlocks ...
+func (loader LastBlockValidatingLoader) LoadBlocks(
+	cursor interface{},
+	count int,
+) (
+	blocks BlockGroup,
+	nextCursor interface{},
+	err error,
+) {
+	blocks, nextCursor, err = loader.Loader.LoadBlocks(cursor, count)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(blocks) == 0 {
+		return blocks, nextCursor, nil
+	}
+
+	nextBlocks, _, err := loader.Loader.LoadBlocks(nextCursor, count)
+	if err != nil {
+		const message = "unable to preload the next blocks " +
+			"corresponding to cursor %v (next cursor %v)"
+		return nil, nil, errors.Wrapf(err, message, cursor, nextCursor)
+	}
+
+	var prevBlock *Block
+	var validationMode ValidationMode
+	if len(nextBlocks) == 0 {
+		validationMode = AsFullBlockchain
+	} else {
+		prevBlock = &nextBlocks[0]
+		validationMode = AsBlockchainChunk
+	}
+	if !blocks.IsLastBlockValid(prevBlock, validationMode, loader.Dependencies) {
+		const message = "the last block of the blocks corresponding to cursor %v " +
+			"is not valid"
+		return nil, nil, errors.Errorf(message, cursor)
+	}
+
+	return blocks, nextCursor, nil
 }
