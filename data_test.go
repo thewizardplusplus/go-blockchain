fd39f03caffbf62ac75fd38563c46612bcb4f741
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-12-22 23:00:00 +0300

    Add the tests for the blockchain.universalDataWrapper.String() method

--- data_test.go
+++ data_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestNewData(test *testing.T) {
@@ -11,4 +12,57 @@ func TestNewData(test *testing.T) {
 
 	wantedData := universalDataWrapper{innerData: "test"}
 	assert.Equal(test, wantedData, data)
+}
+
+func Test_universalDataWrapper_String(test *testing.T) {
+	type fields struct {
+		innerData interface{}
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "integer",
+			fields: fields{
+				innerData: 23,
+			},
+			want: "23",
+		},
+		{
+			name: "string",
+			fields: fields{
+				innerData: "test",
+			},
+			want: "test",
+		},
+		{
+			name: "fmt.Stringer",
+			fields: fields{
+				innerData: func() interface{} {
+					stringer := new(MockStringer)
+					stringer.On("String").Return("test")
+
+					return stringer
+				}(),
+			},
+			want: "test",
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			wrapper := universalDataWrapper{
+				innerData: data.fields.innerData,
+			}
+			got := wrapper.String()
+
+			if _, ok := data.fields.innerData.(interface {
+				AssertExpectations(mock.TestingT) bool
+			}); ok {
+				mock.AssertExpectationsForObjects(test, data.fields.innerData)
+			}
+			assert.Equal(test, data.want, got)
+		})
+	}
 }
