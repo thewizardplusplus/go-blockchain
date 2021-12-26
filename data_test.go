4fee13ab58ad921d0a3ea3383fa0ad34229ddb77
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-12-26 23:00:00 +0300

    Add the tests for the blockchain.universalDataWrapper.Equal() method

--- data_test.go
+++ data_test.go
@@ -151,3 +151,107 @@ func Test_universalDataWrapper_MarshalText(test *testing.T) {
 		})
 	}
 }
+
+func Test_universalDataWrapper_Equal(test *testing.T) {
+	type fields struct {
+		innerData interface{}
+	}
+	type args struct {
+		data Data
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		args   args
+		want   assert.BoolAssertionFunc
+	}{
+		{
+			name: "inner data is DataComparer/false",
+			fields: fields{
+				innerData: func() interface{} {
+					stringer := new(MockDataComparer)
+					stringer.
+						On("Equal", mock.AnythingOfType("*blockchain.MockData")).
+						Return(false)
+
+					return stringer
+				}(),
+			},
+			args: args{
+				data: new(MockData),
+			},
+			want: assert.False,
+		},
+		{
+			name: "inner data is DataComparer/true",
+			fields: fields{
+				innerData: func() interface{} {
+					stringer := new(MockDataComparer)
+					stringer.
+						On("Equal", mock.AnythingOfType("*blockchain.MockData")).
+						Return(true)
+
+					return stringer
+				}(),
+			},
+			args: args{
+				data: new(MockData),
+			},
+			want: assert.True,
+		},
+		{
+			name: "data is universalDataWrapper/false",
+			fields: fields{
+				innerData: "test #1",
+			},
+			args: args{
+				data: universalDataWrapper{
+					innerData: "test #2",
+				},
+			},
+			want: assert.False,
+		},
+		{
+			name: "data is universalDataWrapper/true",
+			fields: fields{
+				innerData: "test",
+			},
+			args: args{
+				data: universalDataWrapper{
+					innerData: "test",
+				},
+			},
+			want: assert.True,
+		},
+		{
+			name: "inner data is not DataComparer and data is not universalDataWrapper",
+			fields: fields{
+				innerData: "test",
+			},
+			args: args{
+				data: new(MockData),
+			},
+			want: assert.False,
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			wrapper := universalDataWrapper{
+				innerData: data.fields.innerData,
+			}
+			got := wrapper.Equal(data.args.data)
+
+			for _, mockInstance := range []interface{}{
+				data.fields.innerData,
+				data.args.data,
+			} {
+				if _, ok := mockInstance.(interface {
+					AssertExpectations(mock.TestingT) bool
+				}); ok {
+					mock.AssertExpectationsForObjects(test, mockInstance)
+				}
+			}
+			data.want(test, got)
+		})
+	}
+}
