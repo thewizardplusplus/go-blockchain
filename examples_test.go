9537046527b2e51658e0fcbb1f546328a0aa9445
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-02-11 23:00:00 +0300

    Update the examples

--- examples_test.go
+++ examples_test.go
@@ -50,28 +50,10 @@ func ExampleBlockchain() {
 	// Output:
 	// [
 	//   {
-	//     "Timestamp": "2006-01-02T16:04:05Z",
-	//     "Data": "genesis block",
-	//     "Hash": "248:225:00e26abd9974fcdea4b32eca43c9dc5c67fffa8efd53cebffa9b049fd6c2bb36",
-	//     "PrevHash": ""
-	//   },
-	//   {
-	//     "Timestamp": "2006-01-02T17:04:05Z",
-	//     "Data": "block #0",
-	//     "Hash": "248:198:0058f5dae6ca3451801a276c94862c7cce085e6f9371e50d80ddbb87c1438faf",
-	//     "PrevHash": "248:225:00e26abd9974fcdea4b32eca43c9dc5c67fffa8efd53cebffa9b049fd6c2bb36"
-	//   },
-	//   {
-	//     "Timestamp": "2006-01-02T18:04:05Z",
-	//     "Data": "block #1",
-	//     "Hash": "248:15:002fc891ad012c4a89f7b267a2ec1767415c627ff69b88b90a93be938b026efa",
-	//     "PrevHash": "248:198:0058f5dae6ca3451801a276c94862c7cce085e6f9371e50d80ddbb87c1438faf"
-	//   },
-	//   {
-	//     "Timestamp": "2006-01-02T19:04:05Z",
-	//     "Data": "block #2",
-	//     "Hash": "248:136:003c7def3d467a759fad481c03cadbd62e62b2c5dbc10e4bbb6e1944c158a8be",
-	//     "PrevHash": "248:15:002fc891ad012c4a89f7b267a2ec1767415c627ff69b88b90a93be938b026efa"
+	//     "Timestamp": "2006-01-02T21:04:05Z",
+	//     "Data": "block #4",
+	//     "Hash": "248:173:00b6863763acd6ec77ca3521589d8e68c118efe855657d702783e8e6aee169a9",
+	//     "PrevHash": "248:65:00d5800e119abe44d89469c2161be7f9645d7237697c6d14b4a72717893582fa"
 	//   },
 	//   {
 	//     "Timestamp": "2006-01-02T20:04:05Z",
@@ -80,10 +62,28 @@ func ExampleBlockchain() {
 	//     "PrevHash": "248:136:003c7def3d467a759fad481c03cadbd62e62b2c5dbc10e4bbb6e1944c158a8be"
 	//   },
 	//   {
-	//     "Timestamp": "2006-01-02T21:04:05Z",
-	//     "Data": "block #4",
-	//     "Hash": "248:173:00b6863763acd6ec77ca3521589d8e68c118efe855657d702783e8e6aee169a9",
-	//     "PrevHash": "248:65:00d5800e119abe44d89469c2161be7f9645d7237697c6d14b4a72717893582fa"
+	//     "Timestamp": "2006-01-02T19:04:05Z",
+	//     "Data": "block #2",
+	//     "Hash": "248:136:003c7def3d467a759fad481c03cadbd62e62b2c5dbc10e4bbb6e1944c158a8be",
+	//     "PrevHash": "248:15:002fc891ad012c4a89f7b267a2ec1767415c627ff69b88b90a93be938b026efa"
+	//   },
+	//   {
+	//     "Timestamp": "2006-01-02T18:04:05Z",
+	//     "Data": "block #1",
+	//     "Hash": "248:15:002fc891ad012c4a89f7b267a2ec1767415c627ff69b88b90a93be938b026efa",
+	//     "PrevHash": "248:198:0058f5dae6ca3451801a276c94862c7cce085e6f9371e50d80ddbb87c1438faf"
+	//   },
+	//   {
+	//     "Timestamp": "2006-01-02T17:04:05Z",
+	//     "Data": "block #0",
+	//     "Hash": "248:198:0058f5dae6ca3451801a276c94862c7cce085e6f9371e50d80ddbb87c1438faf",
+	//     "PrevHash": "248:225:00e26abd9974fcdea4b32eca43c9dc5c67fffa8efd53cebffa9b049fd6c2bb36"
+	//   },
+	//   {
+	//     "Timestamp": "2006-01-02T16:04:05Z",
+	//     "Data": "genesis block",
+	//     "Hash": "248:225:00e26abd9974fcdea4b32eca43c9dc5c67fffa8efd53cebffa9b049fd6c2bb36",
+	//     "PrevHash": ""
 	//   }
 	// ]
 }
