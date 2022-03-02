470ee57d6fefd042a49318b7fb218160972c3aee
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-03-02 23:00:00 +0300

    Add the `blockchain.FindDifferences()` function

--- loader.go
+++ loader.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"github.com/pkg/errors"
+)
+
+// ErrNoMatch ...
+var ErrNoMatch = errors.New("no match")
+
 //go:generate mockery --name=Loader --inpackage --case=underscore --testonly
 
 // Loader ...
@@ -9,4 +16,28 @@ type Loader interface {
 		nextCursor interface{},
 		err error,
 	)
+}
+
+// FindDifferences ...
+func FindDifferences(leftLoader Loader, rightLoader Loader, chunkSize int) (
+	leftDifferences BlockGroup,
+	rightDifferences BlockGroup,
+	err error,
+) {
+	leftBlocks, _, err := leftLoader.LoadBlocks(nil, chunkSize)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "unable to load the left blocks")
+	}
+
+	rightBlocks, _, err := rightLoader.LoadBlocks(nil, chunkSize)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "unable to load the right blocks")
+	}
+
+	leftIndex, rightIndex, hasMatch := leftBlocks.FindDifferences(rightBlocks)
+	if !hasMatch {
+		return nil, nil, ErrNoMatch
+	}
+
+	return leftBlocks[:leftIndex], rightBlocks[:rightIndex], nil
 }
