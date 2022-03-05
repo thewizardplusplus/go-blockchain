eade2d655b5f78e4299392e2c6336aa6733ec2bf
thewizardplusplus <thewizardplusplus@yandex.ru>
2022-03-05 23:00:00 +0300

    Move the utility functions from the `proofers/common.go` file to the `proofers/proof_of_work.go` one

--- proofers/proof_of_work.go
+++ proofers/proof_of_work.go
@@ -1,6 +1,7 @@
 package proofers
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"math/big"
 	"strconv"
@@ -56,4 +57,23 @@ func (proofer ProofOfWork) Validate(block blockchain.Block) error {
 	}
 
 	return nil
+}
+
+func makeHash(data string) []byte {
+	hash := sha256.Sum256([]byte(data))
+	return hash[:]
+}
+
+func makeTarget(targetBit int) *big.Int {
+	target := big.NewInt(0)
+	target.SetBit(target, targetBit, 1)
+
+	return target
+}
+
+func isHashFitTarget(hash []byte, target *big.Int) bool {
+	hashAsInt := big.NewInt(0)
+	hashAsInt.SetBytes(hash)
+
+	return hashAsInt.Cmp(target) == -1 // is less
 }
